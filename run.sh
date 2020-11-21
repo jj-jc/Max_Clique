c52875f9c775994ad5db76e539d0d93f0b750875
german_difonzo <german_difonzo@bitbucket.org>
2020-11-21 16:55:03 +0100

    Inicio del proyecto

---
+++ run.sh
@@ -0,0 +1,14 @@
+
+#/usr/bin/sh
+prog=GA-test
+conf=GAconfig_ejemplo_perms
+
+ date > results.txt
+
+for i in  1 2 3 4 5 9 8 8 9 10
+do
+		echo Executing run $i of $prog with configuration file $conf 
+		./$prog >> results.txt
+		echo "************************************" >> results.txt
+done
+
